54d3504bd074b3deaad36030e59ee2da03d22c88
Tin <tindejphachon@gmail.com>
2024-03-01 12:43:24 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="font-space-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ea at
+        vero quas corrupti laboriosam distinctio libero sed deserunt, quis
+        consequatur perspiciatis porro aspernatur vitae consequuntur ab.
+        Necessitatibus, odio quos.
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      // Light mode colors
+      blue: "#0079FF",
+      gray: "#697C9A",
+      "muted-blue": "#4B6A9B",
+      zinc: "#2B3442",
+      "pale-white": "#F6F8FF",
+      "almost-white": "#FEFEFE",
+
+      // Dark mode colors
+      white: "#FFFFFF",
+      black: "#141D2F",
+      "dark-blue": "#1E2A47",
+    },
+    fontSize: {
+      h1: ["26px", { lineHeight: "38px" }],
+      h2: ["22px", { lineHeight: "33px" }],
+      h3: ["16px", { lineHeight: "24px" }],
+      h4: ["13px", { lineHeight: "20px" }],
+      body: ["15px", { lineHeight: "25px" }],
+    },
+    extend: {
+      fontFamily: { "space-mono": ['"Space Mono"', "monospace"] },
+    },
+  },
+  plugins: [],
+};
